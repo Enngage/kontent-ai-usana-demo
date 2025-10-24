19d95c52c963b308c8b7ada3a59f51cd1d5c9e40
Richard Sustek <ennspassion@gmail.com>
2025-10-24 11:44:03 +0200

    Use router link for nav

--- src/app/components/ui/ui-header.component.ts
+++ src/app/components/ui/ui-header.component.ts
@@ -1,8 +1,10 @@
 import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
 
 @Component({
     selector: 'ui-header',
     templateUrl: './ui-header.component.html',
     styleUrl: './ui-header.component.scss',
+    imports: [RouterLink],
 })
 export class UiHeaderComponent { }   
